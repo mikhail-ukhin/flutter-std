864b1cababc94a3f2700330bad9e335978e24f9e
Mikhail Ukhin <mikhail.ukhin@gmail.com>
2019-12-31 19:24:23 +0300

    simple flutter app

---
+++ mi_card_flutter/lib/main.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 60.0,
+                backgroundImage: AssetImage('images/me.jpeg'),
+              ),
+              Text(
+                'Michael Ukhin',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Text(
+                'Software engineer',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    color: Colors.teal[100],
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+7 666 6666666',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'mikhail.ukhin@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'SourceSansPro',
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
